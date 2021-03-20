23987936e8e7195579ccec071d7f913362bbe847
sureshkumar2738 <72073053+sureshkumar2738@users.noreply.github.com>
2021-03-20 12:35:29 +0530

    second commit

---
+++ src/test/java/multiple_dropdown/suresh.java
@@ -0,0 +1,20 @@
+package multiple_dropdown;
+
+import org.testng.annotations.Test;
+
+public class suresh {
+
+	@Test
+	public void setup() {
+		
+	 System.out.println("login is done");
+		
+	}
+	
+	@Test
+	public void teardown() {
+		
+		System.out.println("close is done");
+		
+	}
+}
