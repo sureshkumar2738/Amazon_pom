af9d9cfcc698de626e313df42796d2c22591184c
suresh kumar <pennadasuresh@gmail.com>
2021-05-03 13:57:17 +0530

    initil chnges

---
+++ src/test/java/TestCases_pom/shopping_page.java
@@ -0,0 +1,17 @@
+package TestCases_pom;
+
+import org.apache.commons.logging.Log;
+import org.testng.annotations.Test;
+
+public class shopping_page {
+
+	@Test
+	public void open() {
+		Login is clone();
+	}
+	
+	@Test
+	public void teardown() {
+		Logout is clone();
+	}
+}
